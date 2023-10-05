f192d87f254a036c4904308c2cbd40932ef6f920
razvanzavate <zavaterazvan@gmail.com>
2023-10-05 21:57:42 +0300

    move code to helpers

--- main.py
+++ main.py
@@ -56,22 +56,14 @@ def are_same_files(file1: str, file2: str) -> bool:
         return hashlib.md5(f1.read()).hexdigest() == hashlib.md5(f2.read()).hexdigest()
 
 
-def sync_folders(source: str, replica: str, logger: Logger):
-    """ Synchronizes replica folder from source folder path
+def add_files_to_replica(source: str, replica: str, logger: Logger):
+    """ Copy files from source to replica if it doesn't exist or is not the same
 
     :param source: source path
     :param replica: replica path
     :param logger: Logger object to be used
-    :return:
+    :return: None
     """
-    if not os.path.isdir(source):
-        raise ValueError(f"{source} is not a folder")
-
-    if not os.path.isdir(replica):
-        # also we can create it if it's not required to already exist
-        raise ValueError(f"{replica} is not a folder")
-
-    # adding files from sync to replica
     for cwd, folders, files in os.walk(source):
         for source_folder in folders:
             source_folder_path = os.path.join(cwd, source_folder)
@@ -89,7 +81,15 @@ def sync_folders(source: str, replica: str, logger: Logger):
                 logger.info(f"Copying file {source_file_path} to {replica_file_path}")
                 shutil.copy2(source_file_path, replica_file_path)
 
-    # removing files from replica if they aren't anymore in source
+
+def remove_files_from_replica(source: str, replica: str, logger: Logger):
+    """ Copy files from source to replica if it doesn't exist or is not the same
+
+    :param source: source path
+    :param replica: replica path
+    :param logger: Logger object to be used
+    :return: None
+    """
     for cwd, folders, files in os.walk(replica):
         for replica_folder in folders:
             replica_folder_path = os.path.join(cwd, replica_folder)
@@ -106,6 +106,25 @@ def sync_folders(source: str, replica: str, logger: Logger):
             if not os.path.isfile(source_file_path):
                 logger.info(f"Removing file {replica_file_path}")
                 os.remove(replica_file_path)
+
+
+def sync_folders(source: str, replica: str, logger: Logger):
+    """ Synchronizes replica folder from source folder path
+
+    :param source: source path
+    :param replica: replica path
+    :param logger: Logger object to be used
+    :return: None
+    """
+    if not os.path.isdir(source):
+        raise ValueError(f"{source} is not a folder")
+
+    if not os.path.isdir(replica):
+        # also we can create it if it's not required to already exist
+        raise ValueError(f"{replica} is not a folder")
+
+    add_files_to_replica(source, replica, logger)
+    remove_files_from_replica(source, replica, logger)
 
 
 def main():
